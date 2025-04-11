8f85554eca414340dcfa38462efc9fc73e35eca9
U2UTrainer <82372639+U2UTrainer@users.noreply.github.com>
2025-04-11 10:16:44 +0200

    Committing 2 items from workspace 670ab479-8050-4255-885c-c3dcfdddc462

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,33 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "4bad5fc6-eb28-444a-8a96-1305ccbfd3dc",
+# META       "default_lakehouse_name": "ProductInfo",
+# META       "default_lakehouse_workspace_id": "670ab479-8050-4255-885c-c3dcfdddc462",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "4bad5fc6-eb28-444a-8a96-1305ccbfd3dc"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT * FROM ProductInfo.product LIMIT 10;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
